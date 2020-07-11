969c50b161d9b95b337d44de44272d76f7e3e909
serh11p <git@serh11p.com>
2020-07-11 19:55:57 +0300

    Fix test and example

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,12 @@
 import "react-app-polyfill/ie11";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider, Link } from "../src";
 
-const App = () => {
-  return <div />;
-};
+const App: React.FC = () => (
+  <Provider>
+    <Link to="/">Home</Link>
+  </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Thing } from "../src";
+import { Provider, Link } from "../src";
 
 describe("it", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <Provider>
+        <Link to="/">Home</Link>
+      </Provider>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
